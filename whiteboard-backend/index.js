0ca2cc3322a87fcdc38a84bf33568b114c875bbc
Ankit Singh <ankitsingh2652003@gmail.com>
2025-10-01 20:29:12 +0530

    Initialized backend and frontend

---
+++ whiteboard-backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+export default io;
